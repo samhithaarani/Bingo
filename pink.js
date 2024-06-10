713ec61944f4497de70fb7ff88de3a1c5f05008b
samhithaarani <n180441@rguktn.ac.in>
2024-06-10 22:40:00 +0530

    improving ui for furthersteps like showing choices

--- pink.js
+++ pink.js
@@ -1,3 +1,13 @@
+const Msgel =  document.getElementById("msg")
+const Insel =  document.getElementById("ins")
+const Insgel =  document.getElementById("insg")
+const Coustcol = document.getElementsByClassName("custom-col-sm")
+const myBtn = document.getElementsByClassName("mybtn")
+const Liel = document.getElementsByTagName("li")
+const Cchoice = document.getElementById("cchoice")
+const Uchoice = document.getElementById("uchoice")
+
+
 document.addEventListener('keydown', function(event) {
     const inputs = document.querySelectorAll('input[type="number"]');
     const currentIndex = Array.from(inputs).findIndex(input => input === document.activeElement);
@@ -48,26 +58,58 @@ document.addEventListener('keydown', function(event) {
         input.value = '';
       });
 
-    console.log(inputValues);
+    for (let i=0 ;i<inputValues.length;i++){
+      if(inputValues[i]==""){
+       Msgel.textContent = "Hey! Watch out you should enter some values! or click reset to proceed with deafult values!";
+      }
+    }
+
+    console.log(inputValues)
+    for (let i=0 ; i<inputs.length ;i++){
+      inputs[i].type = "button"
+      Array.from(Coustcol).forEach(mybox => {
+        mybox.style.backgroundColor = "white";
+        Insel.style.display = "none"
+        Insgel.textContent = "Game Started! Now! computer will give you a number and also u need to clcik on the number you want to select after that!"
+        Insgel.style.color = "blue"
+        Array.from(Liel).forEach(
+          li =>{
+            li.style.display="none"
+            Cchoice.textContent = "Computer choice : "+2
+            Uchoice.textContent = "User choice :" + 3
+
+            
+          }
+        )
+        
+      })
+      
+
+      
+      inputs[i].onclick = play
+    }
   }
 
-
+function play() {
+  console.log("Hello");
+}
 
 
   let inputsdup; 
 
 document.addEventListener('DOMContentLoaded', function() {
   let inputs = document.querySelectorAll('input[type="number"]');
-  inputsdup = Array.from(inputs).map(input => input.value); // Initialize inputsdup inside
+  inputsdup = Array.from(inputs).map(input => input.value); 
 });
 
 function Reset() {
+
   let inputs = document.querySelectorAll('input[type="number"]');
   inputs.forEach((input, index) => {
     if (input.value === '') {
       input.value = inputsdup[index]; 
+      Msgel.textContent = ""
     } else {
-      console.log(`Setting input ${index} to empty value`);
       inputsdup[index] = input.value; 
       input.value = ''; 
     }
